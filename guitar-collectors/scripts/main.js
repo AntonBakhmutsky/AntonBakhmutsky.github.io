89e74895f97164995ede060c4e8d4863bd70989d
AntonBakhmutsky <Anton_bakh@mail.ru>
2020-01-27 20:32:09 +0300

    added guitar collectors test version

--- guitar-collectors/scripts/main.js
+++ guitar-collectors/scripts/main.js
@@ -1,3 +1,13 @@
 window.addEventListener('load', () => {
-  
+  //faq tabs
+  const questions = document.querySelectorAll('.question__head');
+
+  function toggleContent() {
+    console.log(this)
+    this.closest('.question').classList.toggle('question_active');
+    const content = this.closest('.question').children[1];
+    (content.style.maxHeight) ? content.style.maxHeight = null : content.style.maxHeight = content.scrollHeight + 'px';
+  }
+
+  questions.forEach(el => el.addEventListener('click', toggleContent));
 });
