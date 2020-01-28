94a3c73bfb8d1b5b4fab6742355919a70e9d74bb
AntonBakhmutsky <Anton_bakh@mail.ru>
2020-01-28 21:57:05 +0300

    guitar collectors v1.1

--- guitar-collectors/scripts/main.js
+++ guitar-collectors/scripts/main.js
@@ -1,13 +1,36 @@
-window.addEventListener('load', () => {
+window.addEventListener('DOMContentLoaded', () => {
+  //mobile navigation
+  const menuBtn = document.querySelector('.header__menu');
+  const menu = document.querySelector('.mobile__menu');
+
+  const toggleMenu = () => {
+    menuBtn.classList.toggle('header__menu_active');
+    menu.classList.toggle('mobile__menu_active');
+  }
+
+  menuBtn.addEventListener('click', toggleMenu);
+
   //faq tabs
   const questions = document.querySelectorAll('.question__head');
 
   function toggleContent() {
-    console.log(this)
     this.closest('.question').classList.toggle('question_active');
     const content = this.closest('.question').children[1];
     (content.style.maxHeight) ? content.style.maxHeight = null : content.style.maxHeight = content.scrollHeight + 'px';
   }
 
   questions.forEach(el => el.addEventListener('click', toggleContent));
+
+  //to top
+  const upBtn = document.querySelector('.footer__upLink');
+
+  const toTop = () => {
+    event.preventDefault();
+    window.scroll({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+
+  upBtn.addEventListener('click', toTop);
 });
