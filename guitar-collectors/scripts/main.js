fc6dcf93e4eb75f825f96bff6d4b2133cc7b6c6c
AntonBakhmutsky <Anton_bakh@mail.ru>
2020-02-01 14:05:54 +0300

    edits guitar-collectors

--- guitar-collectors/scripts/main.js
+++ guitar-collectors/scripts/main.js
@@ -1,7 +1,20 @@
 window.addEventListener('DOMContentLoaded', () => {
+  //vk icon
+  const vk = document.querySelector('#vk');
+  if (window.innerWidth < 1025) {
+    vk.removeAttribute('target');
+  }
+  window.addEventListener('resize', () => {
+      if (window.innerWidth < 1025) {
+      vk.removeAttribute('target');
+    } else if (window.innerWidth > 1024 && !vk.hasAttribute('target')) {
+      vk.setAttribute('target', '_blank');
+    }
+  });
   //mobile navigation
   const menuBtn = document.querySelector('.header__menu');
   const menu = document.querySelector('.mobile__menu');
+  const faqBtn = document.querySelector('.mobile__navItem[href="#faq"]');
 
   const toggleMenu = () => {
     menuBtn.classList.toggle('header__menu_active');
@@ -9,6 +22,8 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   menuBtn.addEventListener('click', toggleMenu);
+  faqBtn.addEventListener('click', toggleMenu);
+  
 
   //faq tabs
   const questions = document.querySelectorAll('.question__head');
@@ -26,14 +41,36 @@ window.addEventListener('DOMContentLoaded', () => {
 
   const toTop = () => {
     event.preventDefault();
-    window.scroll({
-      top: 0,
-      behavior: 'smooth'
-    });
+    if (window.innerWidth > 1024) {
+      window.scroll({
+        top: 0,
+        behavior: 'smooth'
+      });
+    } else {
+      window.scroll({
+        top: 0
+      });
+    }
   }
 
   upBtn.addEventListener('click', toTop);
 
-  //photo modal
+  //more buttons
+  const buttons = document.querySelectorAll('.howWork__desc .item__more');
+
+  function toQuestion() {
+    event.preventDefault();
+    const question = document.querySelector(`#${this.id.split('_')[1]}`);
+    const content = question.children[1];
+
+    window.scrollTo(question.offsetLeft, question.offsetTop - 100);
+
+    if (!question.classList.contains('question_active')) {
+      question.classList.add('question_active');
+      (content.style.maxHeight) ? content.style.maxHeight = null : content.style.maxHeight = content.scrollHeight + 'px';
+    }
+  }
+
+  buttons.forEach(el => el.addEventListener('click', toQuestion));
   
 });
