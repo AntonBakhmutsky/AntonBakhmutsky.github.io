cd000665622203595468ab64b975f6edff5e1b6b
AntonBakhmutsky <Anton_bakh@mail.ru>
2020-12-03 17:57:20 +0300

    edits

--- test/scripts/tabs.js
+++ test/scripts/tabs.js
@@ -2,71 +2,74 @@ window.addEventListener('load', () => {
   // tabs
   const result = new UAParser().getResult()
 
-  alert(result.os.name)
-
-  // const tabs = document.querySelectorAll('.tab__btn')
-  // const tabsContainer = document.querySelector('.tab__container')
-  // const activeClass = (document.querySelector('.tab__btn').classList.contains('tab__btn_gradientTheme')) ? 'tab__btn_active_gradientTheme' : 'tab__btn_active'
-  // let winWidth
-  // let left
+  const tabs = document.querySelectorAll('.tab__btn')
+  const tabsContainer = document.querySelector('.tab__container')
+  const activeClass = (document.querySelector('.tab__btn').classList.contains('tab__btn_gradientTheme')) ? 'tab__btn_active_gradientTheme' : 'tab__btn_active'
+  let winWidth
+  let left
   
-  // const getRounding = (num) => Math.ceil(num)
+  const getRounding = (num) => Math.ceil(num)
 
-  // function shiftAnimation(direction, x) {
-  //   let draw = setInterval(() => {
-  //     if (direction === 'right') {
-  //       tabsContainer.scrollLeft += 10
+  function shiftAnimation(direction, x) {
+    let draw = setInterval(() => {
+      if (direction === 'right') {
+        tabsContainer.scrollLeft += 10
 
-  //       if (tabsContainer.scrollLeft >= x) {
-  //         clearInterval(draw)
-  //       }
-  //     } else if (direction === 'left') {
-  //       tabsContainer.scrollLeft -= 10
+        if (tabsContainer.scrollLeft >= x) {
+          clearInterval(draw)
+        }
+      } else if (direction === 'left') {
+        tabsContainer.scrollLeft -= 10
 
-  //       if (tabsContainer.scrollLeft <= x) {
-  //         clearInterval(draw)
-  //       }
-  //     }
+        if (tabsContainer.scrollLeft <= x) {
+          clearInterval(draw)
+        }
+      }
 
-  //   }, 20)
-  // }
+    }, 20)
+  }
   
-  // function chooseTab(event) {
-  //   const tab = event.currentTarget
-  //   left = tabsContainer.scrollLeft
+  function chooseTab(event) {
+    const tab = event.currentTarget
+    left = tabsContainer.scrollLeft
 
-  //   if (tab.classList.contains(activeClass)) {
-  //     return false
-  //   }
+    if (tab.classList.contains(activeClass)) {
+      return false
+    }
 
-  //   tabs.forEach(el => el.classList.remove(activeClass))
-  //   tab.classList.add(activeClass)    
+    tabs.forEach(el => el.classList.remove(activeClass))
+    tab.classList.add(activeClass)    
     
-  //   setTimeout(() => {
-  //       const rect = tab.getBoundingClientRect()
-  //       winWidth = window.innerWidth
-      
-  //       const leftX = getRounding(rect.x) 
-  //       const width = getRounding(rect.width)
-  //       const rightX = leftX + width
-      
-  //       if (rightX > winWidth) {
-  //         shift = left + rightX - winWidth + 10
-  //         if (['mobile', 'tablet'].includes(result.device.type) &&) {
-  //           shiftAnimation('right', shift)
-  //         } else {
-  //           tabsContainer.scrollLeft = shift
-  //         }
-  //       } else if (leftX < 0) {
-  //         shift = left + leftX - 10
-  //         if (isIpad || isIphone) {
-  //           shiftAnimation('left', shift)
-  //         } else {
-  //           tabsContainer.scrollLeft = shift
-  //         }
-  //       }    
-  //     }, 300)
-  // }
+    setTimeout(() => {
+        const rect = tab.getBoundingClientRect()
+        winWidth = window.innerWidth
 
-  // tabs.forEach(el => el.addEventListener('click', chooseTab))
+        const leftX = rect.x 
+        const width = rect.width
+        const rightX = leftX + width
+      
+        if (rightX > winWidth) {
+          shift = left + rightX - winWidth + winWidth / 2 - width / 2
+
+          if (['mobile', 'tablet'].includes(result.device.type) && result.os.name === 'iOS') {
+            alert('true')
+            shiftAnimation('right', shift)
+          } else {
+            tabsContainer.scrollLeft = shift
+          }
+
+        } else if (leftX < 0) {
+          shift = left + leftX - 10 - winWidth / 2 + width / 2
+
+          if (['mobile', 'tablet'].includes(result.device.type) && result.os.name === 'iOS') {
+            alert('true')
+            shiftAnimation('left', shift)
+          } else {
+            tabsContainer.scrollLeft = shift
+          }
+        }    
+      }, 300)
+  }
+
+  tabs.forEach(el => el.addEventListener('click', chooseTab))
 });
