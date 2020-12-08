7bbff26e6251f5843e9a011c0cf2a4e1f34f5562
AntonBakhmutsky <Anton_bakh@mail.ru>
2020-12-08 16:01:06 +0300

    process

---
+++ concert/src/scripts/plugins/vh.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', () => {
+  let lastWidth = window.innerWidth
+
+  const listener = () => {
+    let vh = window.innerHeight * 0.01
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
+  }
+
+  window.addEventListener('resize', () => {
+    if (lastWidth !== window.innerWidth) {
+      lastWidth = window.innerWidth
+      listener()
+    }
+  })
+
+  listener()
+})
