ea447b1430386ec938d7ab532b478ef9bc4a8ff8
Anton Bakhmutsky <anton_bakh@mail.ru>
2022-03-16 23:03:42 +0300

    added filter block

---
+++ avtograph/src/scripts/components/dropdown.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', () => {
+  const dropdown = document.querySelector('.filter__dropdown');
+  const options = dropdown.querySelectorAll('.filter__dropdown-options-item')
+  const dropdownBtn = dropdown.querySelector('.filter__value')
+
+  const toggleDropdown = () => dropdown.classList.toggle('filter__dropdown_open')
+
+  dropdownBtn.addEventListener('click', toggleDropdown)
+  options.forEach(el => el.addEventListener('click', toggleDropdown))
+})
--- avtograph/src/scripts/main.js
+++ avtograph/src/scripts/main.js
@@ -1,1 +1,2 @@
 require('./components/user-modal')
+require('./components/dropdown')
