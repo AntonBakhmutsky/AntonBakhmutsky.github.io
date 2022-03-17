e90b61bda0a5e367a53829dc5e1b0773d5a32097
Anton Bakhmutsky <anton_bakh@mail.ru>
2022-03-17 11:34:40 +0300

    added filter logic

--- avtograph/src/scripts/components/dropdown.js
+++ avtograph/src/scripts/components/dropdown.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', () => {
+  // dropdown
   const dropdown = document.querySelector('.filter__dropdown');
-  const options = dropdown.querySelectorAll('.filter__dropdown-options-item');
+  const options = dropdown.querySelectorAll('.filter__options-item');
   const dropdownBtn = dropdown.querySelector('.filter__value');
 
   const toggleDropdown = () => dropdown.classList.toggle('filter__dropdown_open');
@@ -13,4 +14,46 @@ window.addEventListener('load', () => {
       toggleDropdown();
     }
   })
+
+  // filter
+  const optionActiveClass = 'filter__options-item_active';
+  const itemHiddenClass = 'list__item_hidden';
+  const itemClosedClass = 'list__item_closed';
+
+  function filterOutItems(event) {
+    const option = event.currentTarget;
+    const id = +option.dataset.id;
+    const txt = option.textContent;
+    const items = document.querySelectorAll('.list__item');
+
+    options.forEach(el => el.classList.remove(optionActiveClass));
+    option.classList.add(optionActiveClass);
+    dropdownBtn.textContent = txt;
+
+    switch (id) {
+      case 0:
+        items.forEach(el => el.classList.remove(itemHiddenClass));
+        break
+      case 1:
+        items.forEach(el => {
+          if (el.classList.contains(itemClosedClass)) {
+            el.classList.remove(itemHiddenClass);
+          } else {
+            el.classList.add(itemHiddenClass)
+          }
+        });
+        break
+      case 2:
+        items.forEach(el => {
+          if (!el.classList.contains(itemClosedClass)) {
+            el.classList.remove(itemHiddenClass);
+          } else {
+            el.classList.add(itemHiddenClass);
+          }
+        });
+        break
+    }
+  }
+
+  options.forEach(el => el.addEventListener('click', filterOutItems));
 })
