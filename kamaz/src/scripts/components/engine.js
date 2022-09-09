771e50e98cf405777dbcbb85201da82751c5740a
Anton Bakhmutsky <anton_bakh@mail.ru>
2022-09-09 14:21:34 +0300

    clear work tree

--- kamaz/src/scripts/components/engine.js
+++ kamaz/src/scripts/components/engine.js
@@ -1,6 +1,5 @@
 import lottieWeb from 'lottie-web';
 import checkPosition from '@/scripts/plugins/chek-position';
-import lottieInteractivity from "@lottiefiles/lottie-interactivity";
 
 window.addEventListener('load', () => {
 
@@ -10,46 +9,34 @@ window.addEventListener('load', () => {
 
     // lottie animation
     const lottieContainer = document.querySelector('.engine__lottie-container');
-    // const animationData = require('@/assets/documents/R6-engine-animation_2.mp4.lottie-1.json');
-    // let scrollPosition = 0, timer = null;
-    //
-    // const animation = lottieWeb.loadAnimation({
-    //   container: lottieContainer,
-    //   animationData: animationData,
-    //   renderer: 'svg',
-    //   loop: false,
-    //   autoplay: false
-    // });
-    //
-    // window.addEventListener('scroll', () => {
-    //   if (checkPosition(lottieContainer) && window.scrollY > scrollPosition) {
-    //     scrollPosition = window.scrollY;
-    //     animation.setSpeed(1);
-    //     animation.play();
-    //   } else if (checkPosition(lottieContainer) && window.scrollY < scrollPosition) {
-    //     scrollPosition = window.scrollY;
-    //     animation.setSpeed(-1);
-    //     animation.play()
-    //   } else {
-    //     animation.stop();
-    //   }
-    // });
-    //
-    // lottieContainer.addEventListener('click', () => {
-    //   animation.setSpeed(-1)
-    //   animation.play()
-    // })
+    const animationData = require('@/assets/documents/R6-engine-animation_2.mp4.lottie-1.json');
+    let scrollPosition = 0, timer = null;
 
-    LottieInteractivity.create({
-      player: '.engine__lottie-container',
-      mode: 'scroll',
-      actions: [
-        {
-          visibility: [0,1],
-          type: 'seek',
-          frames: [0, 300],
-        },
-      ]
+    const animation = lottieWeb.loadAnimation({
+      container: lottieContainer,
+      animationData: animationData,
+      renderer: 'svg',
+      loop: false,
+      autoplay: false
     });
+
+    window.addEventListener('scroll', () => {
+      if (checkPosition(lottieContainer) && window.scrollY > scrollPosition) {
+        scrollPosition = window.scrollY;
+        animation.setSpeed(1);
+        animation.play();
+      } else if (checkPosition(lottieContainer) && window.scrollY < scrollPosition) {
+        scrollPosition = window.scrollY;
+        animation.setSpeed(-1);
+        animation.play()
+      } else {
+        animation.stop();
+      }
+    });
+
+    lottieContainer.addEventListener('click', () => {
+      animation.setSpeed(-1)
+      animation.play()
+    })
   }
 });
