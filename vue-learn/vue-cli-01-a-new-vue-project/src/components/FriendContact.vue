1de1c12ec78fe3fba0b3690a136553a63362d7be
Anton Bakhmutsky <anton_bakh@mail.ru>
2022-09-19 17:59:42 +0300

    clear work tree

---
+++ vue-learn/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+
+li
+  h2 {{ name }} {{ friendIsFavorite ? '(Favorite)' : '' }}
+  button(@click="toggleDetails") Show Details
+  button(@click="toggleFavorite") Toggle Favorite
+  ul(v-if="detailsAreVisible")
+    li #[strong Phone: {{ phoneNumber }}]
+    li #[strong Email: {{ emailAddress }}]
+
+</template>
+
+<script>
+export default {
+  props: [
+    'name',
+    'phoneNumber',
+    'emailAddress',
+    'isFavorite'
+  ],
+  data() {
+    return {
+      detailsAreVisible: false,
+      friendIsFavorite: this.isFavorite
+    }
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      this.friendIsFavorite = !this.friendIsFavorite;
+    }
+  },
+}
+</script>
