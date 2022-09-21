6d806df16ce43a64dd8106a59aeaa1f03b38f444
Anton Bakhmutsky <anton_bakh@mail.ru>
2022-09-21 10:27:42 +0300

    clear work tree

---
+++ vue-learn/prj-cmp-01-starting-setup/src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+
+li
+  div
+    header
+      h3 {{ title }}
+      button Delete
+  p {{ description }}
+  nav
+    a(:href="link")
+
+</template>
+
+<script>
+export default {
+  props: ['title', 'description', 'link'],
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+li
+  margin: auto
+  max-width: 40rem
+
+header
+  display: flex
+  justify-content: space-between
+  align-items: center
+
+h3
+  font-size: 1.25rem
+  margin: 0.5rem 0
+
+p
+  margin: 0.5rem 0
+
+a
+  text-decoration: none
+  color: #ce5c00
+
+  &:hover, &:active
+    color: #c89300
+</style>
---
+++ vue-learn/prj-cmp-01-starting-setup/src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+
+ul
+  LearningResource(
+    v-for="res in resources"
+    :key="res.id"
+    :title="res.title"
+    :description="res.description"
+    :link="res.link"
+  )
+
+</template>
+
+<script>
+import LearningResource from './components/learning-resources/LearningResource';
+
+export default {
+  components: {
+    LearningResource
+  },
+  props: ['resources'],
+}
+</script>
+
+<style lang="sass" scoped>
+ul
+  list-style: none
+  margin: 0 auto
+  padding: 0
+  max-width: 40rem
+</style>
