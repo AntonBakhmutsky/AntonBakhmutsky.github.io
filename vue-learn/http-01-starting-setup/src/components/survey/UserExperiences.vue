72fa0f9e8665e9e5f78cbe375ee437deb3875a57
Anton Bakhmutsky <anton_bakh@mail.ru>
2022-09-23 15:31:54 +0300

    clear work tree

--- vue-learn/http-01-starting-setup/src/components/survey/UserExperiences.vue
+++ vue-learn/http-01-starting-setup/src/components/survey/UserExperiences.vue
@@ -1,11 +1,13 @@
 <template lang="pug">
-BasePreloader(v-if="isLoading")
 section
   BaseCard
     h2 Submitted Experiences
     div
       base-button(@click="loadExperiences") Load Submitted Experiences
-    ul
+    p(v-if="isLoading") Loading...
+    p(v-else-if="!isLoading && error" ) {{ error }}
+    P(v-else-if="!isLoading && (!results || results.length === 0)" ) No stored experiences found. Start adding some survey results first.
+    ul(v-else-if="!isLoading && results && results.length > 0")
       survey-result(
         v-for="result in results"
         :key="result.id"
@@ -24,12 +26,14 @@ export default {
   data() {
     return {
       results: [],
-      isLoading: false
+      isLoading: false,
+      error: null
     }
   },
   methods: {
     loadExperiences() {
       this.isLoading = true;
+      this.error = null;
       fetch('https://vue-http-demo-ca7ca-default-rtdb.firebaseio.com/surveys.json').then(function(response) {
         if (response.ok) {
           return response.json();
@@ -41,6 +45,10 @@ export default {
           results.push({id: id, name: data[id].name, rating: data[id].rating});
         }
         this.results = results;
+      }).catch((error) => {
+        console.log(error)
+        this.isLoading = false;
+        this.error = 'Filed to fetch data - please try again later';
       });
     }
   },
