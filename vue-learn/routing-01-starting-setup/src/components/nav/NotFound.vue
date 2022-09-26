43b19952c6da6668c4f774357af599c03f4673a8
Anton Bakhmutsky <anton_bakh@mail.ru>
2022-09-26 10:21:23 +0300

    clear work tree

---
+++ vue-learn/routing-01-starting-setup/src/components/nav/NotFound.vue
@@ -0,0 +1,8 @@
+<template lang="pug">
+h2 Page notfound
+RouterLink(to="/teams") Home
+</template>
+
+<script>
+export default {}
+</script>
--- vue-learn/routing-01-starting-setup/src/main.js
+++ vue-learn/routing-01-starting-setup/src/main.js
@@ -5,13 +5,19 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList';
 import UsersList from './components/users/UsersList';
 import TeamMembers from './components/teams/TeamMembers';
+import NotFound from "@/components/nav/NotFound";
 
 const router = createRouter({
    history: createWebHistory(),
    routes: [
       {
+         path: '/',
+         redirect: '/teams'
+      },
+      {
          path: '/teams',
-         component: TeamsList
+         component: TeamsList,
+         // alias: '/'
       },
       {
          path: '/users',
@@ -22,6 +28,10 @@ const router = createRouter({
          component: TeamMembers,
          props: true
       },
+      {
+         path: '/:notFound(.*)',
+         component: NotFound
+      }
    ],
    linkActiveClass: 'active'
 });
