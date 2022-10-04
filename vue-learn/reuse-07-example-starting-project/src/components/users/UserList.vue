fcf9b70c0da1ad788866f9dc435273414b537db6
Anton Bakhmutsky <anton_bakh@mail.ru>
2022-10-04 15:20:14 +0300

    clear work tree

--- vue-learn/reuse-07-example-starting-project/src/components/users/UserList.vue
+++ vue-learn/reuse-07-example-starting-project/src/components/users/UserList.vue
@@ -10,9 +10,9 @@ BaseContainer
 </template>
 
 <script>
-import { ref, computed, toRefs } from 'vue';
+import { toRefs } from 'vue';
 import useSearch from "@/hook/search";
-
+import useSorting from "@/hook/sorting";
 import UserItem from './UserItem.vue';
 
 export default {
@@ -26,32 +26,12 @@ export default {
 
     const {enteredSearchTerm, availableItems, updateSearch} = useSearch(users, 'fullName');
 
-    const sorting = ref(null);
-    const displayedUsers = computed(function () {
-      if (!sorting.value) {
-        return availableItems.value;
-      }
-      return availableItems.value.slice().sort((u1, u2) => {
-        if (sorting.value === 'asc' && u1.fullName > u2.fullName) {
-          return 1;
-        } else if (sorting.value === 'asc') {
-          return -1;
-        } else if (sorting.value === 'desc' && u1.fullName > u2.fullName) {
-          return -1;
-        } else {
-          return 1;
-        }
-      });
-    });
-
-    function sort(mode) {
-      sorting.value = mode;
-    }
+    const {displayedItems, sorting, sort} = useSorting(availableItems, 'fullName');
 
     return {
       enteredSearchTerm,
       updateSearch,
-      displayedUsers,
+      displayedUsers: displayedItems,
       sorting,
       sort
     };
