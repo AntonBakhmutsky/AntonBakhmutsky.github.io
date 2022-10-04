ee1e01549758472927bbf52d48e1f2e8316c06db
Anton Bakhmutsky <anton_bakh@mail.ru>
2022-10-04 10:32:20 +0300

    clear work tree

--- vue-learn/reuse-04-composition-api-starting-project/src/components/DeleteUser.vue
+++ vue-learn/reuse-04-composition-api-starting-project/src/components/DeleteUser.vue
@@ -7,28 +7,16 @@ section
 </template>
 
 <script>
-import { ref } from 'vue';
 import UserAlert from './UserAlert.vue';
+import useAlert from '@/hooks/alert';
 
 export default {
   components: {
     UserAlert,
   },
   setup() {
-    const alertIsVisible = ref(false);
-
-    function showAlert() {
-      alertIsVisible.value = true;
-    }
-    function hideAlert() {
-      alertIsVisible.value = false;
-    }
-
-    return {
-      alertIsVisible,
-      showAlert,
-      hideAlert,
-    };
-  },
+    const [alertIsVisible, showAlert, hideAlert] = useAlert(true);
+    return {alertIsVisible, showAlert, hideAlert};
+  }
 };
 </script>
