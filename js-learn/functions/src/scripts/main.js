5aff2190dbb79c5636fe5f4c96d1dcbd6855e8da
Anton Bakhmutsky <anton_bakh@mail.ru>
2022-10-14 16:42:35 +0300

    clear work tree

--- js-learn/functions/src/scripts/main.js
+++ js-learn/functions/src/scripts/main.js
@@ -1,35 +1,39 @@
-const myself = {
-  name: 'Max',
-  friends: [
-    {
-      name: 'Manuel',
-      friends: [
-        {name: 'Hari'},
-        {name: 'Chris'},
-      ]
-    },
-    {
-      name: 'Julia',
-      friends: [
-        {name: 'Harold'}
-      ]
-    }
-  ]
-}
+// functions
+// const myself = {
+//   name: 'Max',
+//   friends: [
+//     {
+//       name: 'Manuel',
+//       friends: [
+//         {name: 'Hari'},
+//         {name: 'Chris'},
+//       ]
+//     },
+//     {
+//       name: 'Julia',
+//       friends: [
+//         {name: 'Harold'}
+//       ]
+//     }
+//   ]
+// }
+//
+// function getFriendNames(person) {
+//   const collectedNames = [];
+//
+//   if (!person.friends) {
+//     return;
+//   }
+//
+//   for (const friend in person.friends) {
+//     collectedNames.push(friend.name);
+//     collectedNames.push(...getFriendNames(friend));
+//   }
+//
+//   return collectedNames;
+// }
+//
+// console.log(getFriendNames(myself));
 
-function getFriendNames(person) {
-  const collectedNames = [];
 
-  if (!person.friends) {
-    return;
-  }
-
-  for (const friend in person.friends) {
-    collectedNames.push(friend.name);
-    collectedNames.push(...getFriendNames(friend));
-  }
-
-  return collectedNames;
-}
-
-console.log(getFriendNames(myself));
+// numbers
