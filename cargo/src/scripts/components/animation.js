9bfbfb69e5fbcd28f8eba75331b35add734bc2cb
Anton Bakhmutsky <anton_bakh@mail.ru>
2022-10-27 17:42:50 +0300

    clear work tree

--- cargo/src/scripts/components/animation.js
+++ cargo/src/scripts/components/animation.js
@@ -3,6 +3,36 @@ import anime from 'animejs';
 
 window.addEventListener('load', () => {
 
+  // slide to right
+  const slideToRightElements = document.querySelectorAll('.slide-right');
 
+  slideToRightElements.forEach(el => {
+    inViewport(el, () => {
+      anime({
+        targets: el,
+        opacity: [0, 1],
+        translateX: [-110, 0],
+        duration: 1000,
+        easing: 'easeOutQuart',
+        delay: Number(el.dataset.delay) || 0
+      })
+    });
+  });
+
+  // slide to up
+  const slideToUpElements = document.querySelectorAll('.slide-up');
+
+  slideToUpElements.forEach(el => {
+    inViewport(el, () => {
+      anime({
+        targets: el,
+        opacity: [0, 1],
+        translateY: [130, 0],
+        duration: 1000,
+        easing: 'easeOutQuart',
+        delay: Number(el.dataset.delay) || 0
+      })
+    });
+  });
 
 });
