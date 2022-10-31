56feb52475a378647baf4414b8dcac7a5c112e2d
Anton Bakhmutsky <anton_bakh@mail.ru>
2022-10-31 14:37:24 +0300

    clear work tree

--- cargo/src/scripts/components/production.js
+++ cargo/src/scripts/components/production.js
@@ -3,8 +3,31 @@ import Swiper, {Navigation} from 'swiper';
 window.addEventListener('load', () => {
   const productionSlider = new Swiper('.production__slider.swiper', {
     modules: [Navigation],
-    slidesPerView: 3.16,
-    spaceBetween: 15,
-    loop: true
+    loop: true,
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 11
+      },
+      520: {
+        slidesPerView: 1.5,
+        spaceBetween: 15
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 15
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 15,
+      },
+      1224: {
+        slidesPerView: 3.16,
+        spaceBetween: 15
+      }
+    },
+    navigation: {
+      nextEl: '.swiper-button-next'
+    }
   })
 });
