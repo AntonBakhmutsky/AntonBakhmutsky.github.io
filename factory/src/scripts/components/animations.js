617e0905873ab37de92ab27c828ae91cff78d7d7
Anton Bakhmutsky <anton_bakh@mail.ru>
2022-11-15 15:15:52 +0300

    clear work tree

--- factory/src/scripts/components/animations.js
+++ factory/src/scripts/components/animations.js
@@ -1,3 +1,55 @@
+import anime from 'animejs'
+import inViewport from '../helpers/inViewport'
+
 window.addEventListener('load', () => {
-  document.querySelectorAll('a').forEach(el => el.addEventListener('click', e => e.preventDefault()))
+  // slide to right
+  const slideToRightElements = document.querySelectorAll('.slide-right')
+
+  slideToRightElements.forEach(el => {
+    inViewport(el, () => {
+      anime({
+        targets: el,
+        opacity: [0, 1],
+        translateX: [-130, 0],
+        duration: 1000,
+        easing: 'easeOutQuart'
+      })
+    })
+  })
+
+  // slide to up
+  const slideToUpElements = document.querySelectorAll('.slide-up')
+
+  slideToUpElements.forEach(el => {
+    inViewport(el, () => {
+      anime({
+        targets: el,
+        opacity: [0, 1],
+        translateY: [130, 0],
+        duration: 1000,
+        easing: 'easeOutQuart'
+      })
+    })
+  })
+
+  // nums animation
+  if (!document.querySelector('.anim-num')) {
+    return false
+  } else {
+
+    const nums = document.querySelectorAll('.anim-num')
+
+    nums.forEach(num => {
+      const numValue = Number(num.textContent.split(' ').join(''))
+
+      inViewport(num, () => {
+        anime({
+          targets: num,
+          innerHTML: [0, numValue],
+          easing: 'linear',
+          round: 1
+        })
+      })
+    })
+  }
 })
