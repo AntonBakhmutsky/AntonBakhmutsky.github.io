2f6d836cecfc3dd4f501f2ee2099c7db9cb9f4f2
Anton Bakhmutsky <anton_bakh@mail.ru>
2022-11-17 14:22:36 +0300

    clear work tree

--- factory/src/scripts/components/animations.js
+++ factory/src/scripts/components/animations.js
@@ -10,7 +10,7 @@ window.addEventListener('load', () => {
       anime({
         targets: el,
         opacity: [0, 1],
-        translateX: [-130, 0],
+        translateX: [-150, 0],
         duration: 1000,
         easing: 'easeOutQuart'
       })
@@ -25,7 +25,7 @@ window.addEventListener('load', () => {
       anime({
         targets: el,
         opacity: [0, 1],
-        translateY: [130, 0],
+        translateY: [150, 0],
         duration: 1000,
         easing: 'easeOutQuart'
       })
@@ -40,7 +40,7 @@ window.addEventListener('load', () => {
       anime({
         targets: el,
         opacity: [0, 1],
-        translateX: [130, 0],
+        translateX: [150, 0],
         duration: 1000,
         easing: 'easeOutQuart'
       })
@@ -67,81 +67,4 @@ window.addEventListener('load', () => {
       })
     })
   }
-
-  // // list left animation
-  // function leftListAnimation(selector, delay = 150, arr = null) {
-  //   if (selector) {
-  //     if (!document.querySelector(selector)) {
-  //       return false
-  //     } else {
-  //       const items = document.querySelectorAll(selector)
-  //
-  //       items.forEach((el, i) => {
-  //         inViewport(el, () => {
-  //           anime({
-  //             targets: el,
-  //             opacity: [0, 1],
-  //             translateX: [130, 0],
-  //             duration: 1000,
-  //             delay: i * delay,
-  //             easing: 'easeOutQuart'
-  //           })
-  //         })
-  //       })
-  //     }
-  //   } else {
-  //     arr.forEach((el, i) => {
-  //       inViewport(el, () => {
-  //         anime({
-  //           targets: el,
-  //           opacity: [0, 1],
-  //           translateX: [130, 0],
-  //           duration: 1000,
-  //           delay: i * delay,
-  //           easing: 'easeOutQuart'
-  //         })
-  //       })
-  //     })
-  //   }
-  // }
-  //
-  // // top list animation
-  // function topListAnimation(selector) {
-  //   if (!document.querySelector(selector)) {
-  //     return false
-  //   } else {
-  //     const items = document.querySelectorAll(selector)
-  //
-  //     items.forEach((el, i) => {
-  //       inViewport(el, () => {
-  //         anime({
-  //           targets: el,
-  //           opacity: [0, 1],
-  //           translateY: [130, 0],
-  //           duration: 1000,
-  //           delay: i * 150,
-  //           easing: 'easeOutQuart'
-  //         })
-  //       })
-  //     })
-  //   }
-  // }
-  //
-  // // header menu
-  // let headerMenuItems = document.querySelector('.header__menu-list').children
-  //
-  // leftListAnimation(null, 100, [...headerMenuItems])
-  //
-  // // partners list
-  // leftListAnimation('.partners__list li')
-  //
-  // // product.pug list
-  // topListAnimation('.products-slider .swiper-slide')
-  //
-  // // news list
-  // leftListAnimation('.index-news__item')
-  //
-  // // why list
-  // topListAnimation('.why__list li', 50)
-
 })
