0c256286fd3f458ae32d6a470a19718073ac0a93
Anton Bakhmutsky <anton_bakh@mail.ru>
2023-01-24 14:14:58 +0300

    clear work tree

--- factory/src/scripts/components/product-slider.js
+++ factory/src/scripts/components/product-slider.js
@@ -4,26 +4,59 @@ window.addEventListener('load', () => {
   if (!document.querySelector('.product-slider .main-slider')) {
     return false
   } else {
-    new Swiper('.product-slider .main-slider', {
-      loop: true
-
-    })
-    new Swiper('.product-slider .thumb-slider', {
-      loop: true,
+    const thumbSlider = new Swiper('.product-slider .swiper.thumb-slider', {
       spaceBetween: 12,
       slidesPerView: 'auto',
       freeMode: true,
       watchSlidesProgress: true
     })
-    new Swiper('.product-slider .main-modal-slider', {
-
-    })
-    new Swiper('.product-slider .thumb-modal-slider', {
-      loop: true,
+    new Swiper('.product-slider .swiper.main-slider', {
+      modules: [Navigation, Pagination, Thumbs],
       spaceBetween: 10,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+      },
+      thumbs: {
+        swiper: thumbSlider,
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1.06,
+        },
+        1025: {
+          slidesPerView: 1
+        }
+      }
+    })
+    const thumbModalSlider = new Swiper('.product-slider .swiper.thumb-modal-slider', {
+      spaceBetween: 6,
       slidesPerView: 'auto',
       freeMode: true,
-      watchSlidesProgress: true
+      watchSlidesProgress: true,
+      breakpoints: {
+        1025: {
+          direction: 'vertical',
+        }
+      }
+    })
+    new Swiper('.product-slider .swiper.main-modal-slider', {
+      modules: [Navigation, Pagination, Thumbs],
+      loop: true,
+      spaceBetween: 200,
+      slidesPerView: 1,
+      centeredSlides: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      thumbs: {
+        swiper: thumbModalSlider,
+      },
     })
   }
 })
