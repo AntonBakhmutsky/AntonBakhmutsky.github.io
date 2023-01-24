4d9a988c912972e41662085f380b8643072a2700
Anton Bakhmutsky <anton_bakh@mail.ru>
2023-01-24 09:22:19 +0300

    clear work tree

--- factory/src/scripts/components/product-slider.js
+++ factory/src/scripts/components/product-slider.js
@@ -1,58 +1,21 @@
-import Swiper, {Navigation} from 'swiper'
-import {topListAnimation} from '../helpers/animations'
+import Swiper, {Navigation, Thumbs, Pagination} from 'swiper'
 
 window.addEventListener('load', () => {
-  if (!document.querySelector('.product-slider .swiper')) {
+  if (!document.querySelector('.product-slider .main-slider')) {
     return false
   } else {
-    const slider = new Swiper('.product-slider .swiper', {
-      modules: [Navigation],
-      navigation: {
-        nextEl: '.swiper-button-next'
-      },
-      loop: true,
-      breakpoints: {
-        320: {
-          slidesPerView: 1,
-        },
-        640: {
-          slidesPerView: 1.2,
-          spaceBetween: 20
-        },
-        768: {
-          slidesPerView: 1.4,
-          spaceBetween: 20
-        },
-        1280: {
-          slidesPerView: 2.3,
-          spaceBetween: 20
-        }
-      }
+    new Swiper('.product-slider .main-slider', {
+      loop: true
+
     })
+    new Swiper('.product-slider .thumb-slider', {
 
-    // products list
-    topListAnimation('.product-slider .swiper-slide:not(.swiper-slide-duplicate)')
-
-    // swiper wrapper height
-    const swiperSlideImgs = document.querySelectorAll('.product-slider .swiper-slide .swiper-slide-image');
-    const swiperWrapper = document.querySelector('.product-slider .swiper-wrapper')
-
-    function setSwiperHeight() {
-      const contentHeights = [];
-
-      swiperSlideImgs.forEach(el => contentHeights.push(el.scrollHeight));
-
-      const maxContentHeight = Math.max(...contentHeights);
-
-      swiperWrapper.style.height = `${maxContentHeight}px`
-    }
-
-    setSwiperHeight()
-
-    window.addEventListener('resize', () => {
-      swiperWrapper.style.height = 'auto'
-      setSwiperHeight()
     })
+    new Swiper('.product-slider .main-modal-slider', {
 
+    })
+    new Swiper('.product-slider .thumb-modal-slider', {
+
+    })
   }
 })
