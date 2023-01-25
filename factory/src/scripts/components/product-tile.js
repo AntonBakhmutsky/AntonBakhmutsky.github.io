d92fa3a1965a0c3a1f0b56064a98721ea7a0f023
Anton Bakhmutsky <anton_bakh@mail.ru>
2023-01-25 12:36:07 +0300

    clear work tree

--- factory/src/scripts/components/product-tile.js
+++ factory/src/scripts/components/product-tile.js
@@ -5,22 +5,5 @@ window.addEventListener('load', () => {
     return false
   } else {
     topListAnimation('.product-tile__item')
-
-    const switcherBtns = document.querySelectorAll('.product-tile__switcher-btn')
-    const grid = document.querySelector('.product-tile__grid')
-
-    const changeDirection = (e) => {
-      if (e.currentTarget.dataset.direction === 'vertical') {
-        grid.classList.remove('product-tile__grid_horizontal')
-        grid.classList.add('product-tile__grid_vertical')
-      } else {
-        grid.classList.remove('product-tile__grid_vertical')
-        grid.classList.add('product-tile__grid_horizontal')
-      }
-      switcherBtns.forEach(el => el.classList.remove('active'))
-      e.currentTarget.classList.add('active')
-    }
-
-    switcherBtns.forEach(el => el.addEventListener('click', changeDirection))
   }
 })
