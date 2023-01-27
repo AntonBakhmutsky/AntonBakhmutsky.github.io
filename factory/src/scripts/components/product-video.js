92e138e6bb2b84f7785c28daef0eb165e84819c4
Anton Bakhmutsky <anton_bakh@mail.ru>
2023-01-27 11:40:33 +0300

    clear work tree

--- factory/src/scripts/components/product-video.js
+++ factory/src/scripts/components/product-video.js
@@ -6,23 +6,12 @@ window.addEventListener('load', () => {
     const videoButtons = document.querySelectorAll('.product__video-btn')
 
     const toggleVideo = (e) => {
-      console.log(e)
       const videoContainer = e.target.closest('.product__video')
       const video = videoContainer.querySelector('video')
 
-      const stopVideo = () => {
-        videoContainer.classList.remove('playing')
-        video.pause()
-      }
+      videoContainer.classList.add('playing')
+      video.play()
 
-      if (videoContainer.classList.contains('playing')) {
-        stopVideo()
-      } else {
-        videoContainer.classList.add('playing')
-        video.play()
-      }
-
-      video.addEventListener('pause', () => stopVideo())
     }
 
     videoButtons.forEach(el => el.addEventListener('click', toggleVideo))
