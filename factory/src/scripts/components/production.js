c35eea49a2817c0ab814067326c7cf0648dd430a
Anton Bakhmutsky <anton_bakh@mail.ru>
2023-01-27 15:59:47 +0300

    clear work tree

---
+++ factory/src/scripts/components/production.js
@@ -0,0 +1,31 @@
+import Swiper, {Navigation, Pagination} from 'swiper'
+
+window.addEventListener('load', () => {
+  if (!document.querySelector('.production-slider')) {
+    return false
+  } else {
+    new Swiper('.production-slider .swiper', {
+      modules: [Navigation, Pagination],
+      loop: true,
+      loopAdditionalSlides: 9,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1.06,
+          spaceBetween: 10
+        },
+        1025: {
+          slidesPerView: 1,
+          spaceBetween: 0
+        }
+      }
+    })
+  }
+})
