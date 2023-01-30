3f7920c6744f78434bdc70340a2454ff78205e70
Anton Bakhmutsky <anton_bakh@mail.ru>
2023-01-30 13:49:21 +0300

    clear work tree

--- factory/src/scripts/components/product-video.js
+++ factory/src/scripts/components/product-video.js
@@ -11,7 +11,6 @@ window.addEventListener('load', () => {
 
       videoContainer.classList.add('playing')
       video.play()
-
     }
 
     videoButtons.forEach(el => el.addEventListener('click', toggleVideo))
