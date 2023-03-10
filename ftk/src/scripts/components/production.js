83cfaaadb6cad86f9da11fe71c843a3bb9505e4d
Anton Bakhmutsky <anton_bakh@mail.ru>
2023-03-10 14:10:05 +0300

    clear work tree

---
+++ ftk/src/scripts/components/production.js
@@ -0,0 +1,24 @@
+import Swiper, {Navigation} from 'swiper'
+
+window.addEventListener('load', () => {
+  new Swiper('.production__bottom .swiper', {
+    modules: [Navigation],
+    loop: true,
+    initialSlide: 1,
+    loopAdditionalSlides: 9,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1.01,
+        spaceBetween: 10
+      },
+      1025: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      }
+    }
+  })
+});
