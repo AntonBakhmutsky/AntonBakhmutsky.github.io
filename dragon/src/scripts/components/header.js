aca30f303bb6d434c60bc1ac8050a6c11b13a60c
Anton Bakhmutsky <anton_bakh@mail.ru>
2023-03-24 12:46:30 +0300

    clear working tree

--- dragon/src/scripts/components/header.js
+++ dragon/src/scripts/components/header.js
@@ -14,4 +14,52 @@ window.addEventListener('load', () => {
 
   menuBtn.addEventListener('click', toggleMenu)
   menuItems.forEach(el => el.addEventListener('click', toggleMenu))
+
+  // sticky header
+  const header = document.querySelector('.header')
+  let scrollPosition = 0
+
+  if (window.scrollY !== 0) {
+    header.classList.add('sticky')
+  }
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > scrollPosition && window.scrollY > 19) {
+      scrollPosition = window.scrollY
+      if (!header.classList.contains('sticky')) {
+        header.classList.add('sticky')
+      }
+      if (header.classList.contains('up')) {
+        header.classList.remove('up')
+      }
+
+    } else  {
+      scrollPosition = window.scrollY;
+
+      if (window.scrollY < 20) {
+        header.classList.remove('sticky')
+        header.classList.remove('up')
+      } else {
+        header.classList.add('up')
+      }
+    }
+  })
+
+  // header search
+  const search = document.querySelector('.header__search-field')
+  const searchBtn = document.querySelector('.header__search-btn')
+
+  const toggleSearch = (e) => {
+    if (!search.classList.contains('active')) {
+      search.classList.add('active')
+      searchBtn.classList.add('disabled')
+    } else {
+      search.classList.remove('active')
+      searchBtn.classList.remove('disabled')
+      e.currentTarget.closest('form').reset()
+    }
+  }
+
+  searchBtn.addEventListener('click', toggleSearch)
+  search.addEventListener('click', toggleSearch)
 });
