e5f0a876200febf80ad0d55fa20307185d6c449d
Anton Bakhmutsky <anton_bakh@mail.ru>
2023-03-29 17:53:58 +0300

    clear working tree

--- dragon/src/scripts/components/main-slider.js
+++ dragon/src/scripts/components/main-slider.js
@@ -4,7 +4,7 @@ window.addEventListener('load', () => {
   if (!document.querySelector('.main-slider')) {
     return false
   } else {
-    new Swiper('.main-slider .swiper',  {
+    const slider = new Swiper('.main-slider .swiper',  {
       modules: [Autoplay, Navigation, Pagination],
       loop: true,
       slidesPerView: 1,
@@ -16,6 +16,16 @@ window.addEventListener('load', () => {
         el: '.swiper-pagination',
         type: 'bullets',
       },
+    })
+
+    const sliderCounterLength = document.querySelector('.swiper-counter-length span')
+    const sliderCounterProgress = document.querySelector('.swiper-counter-progress span')
+
+    sliderCounterLength.textContent = slider.slides.length.toString()
+    sliderCounterProgress.textContent = (slider.realIndex + 1).toString()
+
+    slider.on('slideChange', () => {
+      sliderCounterProgress.textContent = (slider.realIndex + 1).toString()
     })
   }
 })
