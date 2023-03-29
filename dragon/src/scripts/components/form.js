0ef5770993e3e27b32ecfe7402a5dddfdebe71e4
Anton Bakhmutsky <anton_bakh@mail.ru>
2023-03-29 13:35:31 +0300

    clear working tree

--- dragon/src/scripts/components/form.js
+++ dragon/src/scripts/components/form.js
@@ -2,6 +2,7 @@ window.addEventListener('load', () => {
   if (!document.querySelector('.form')) {
     return null
   } else {
+    // placeholder
     const inputs = document.querySelectorAll('.form__field-input')
 
     const risePlaceholder = (e) => {
@@ -17,5 +18,37 @@ window.addEventListener('load', () => {
 
     inputs.forEach(el => el.addEventListener('focus', risePlaceholder))
     inputs.forEach(el => el.addEventListener('blur', lowerPlaceholder))
+  }
+
+  if (!document.querySelector('.form__select')) {
+    return null
+  } else {
+    const selects = document.querySelectorAll('.form__select')
+
+    function handleSelect(e) {
+      this.classList.toggle('open')
+
+      const visibleValue = this.querySelector('.form__select-visible-value')
+      const hiddenValue = this.querySelector('input')
+
+      if (e.target.closest('.form__select-options-item')) {
+        const option = e.target.closest('.form__select-options-item')
+        const value = option.textContent
+        this.querySelectorAll('.form__select-options-item').forEach(el => {
+          el !== option ? el.classList.remove('active'): el.classList.add('active')
+        })
+        visibleValue.textContent = value
+        hiddenValue.value = value
+        this.classList.add('selected')
+      }
+    }
+
+    document.addEventListener('click', (e) => {
+      if (!e.target.closest('.form__select')) {
+        selects.forEach(el => el.classList.remove('open'))
+      }
+    })
+
+    selects.forEach(el => el.addEventListener('click', handleSelect))
   }
 });
