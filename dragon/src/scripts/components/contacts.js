f42760563b3be83c2819fbfd1913565aa766f93c
Anton Bakhmutsky <anton_bakh@mail.ru>
2023-04-13 14:37:09 +0300

    clear working tree

--- dragon/src/scripts/components/contacts.js
+++ dragon/src/scripts/components/contacts.js
@@ -1,4 +1,5 @@
 import Swiper, {Navigation} from 'swiper'
+import anime from 'animejs'
 
 window.addEventListener('load', () => {
   if (!document.querySelector('.contacts')) {
@@ -13,16 +14,12 @@ window.addEventListener('load', () => {
         controls: [],
       })
 
-      const iconContentLayout = ymaps.templateLayoutFactory.createClass(
-        '<div class="map__address">$[properties.iconContent]</div>'
-      )
-
       const redForest = new ymaps.Placemark([59.66768456450318, 30.651756999999925], {
       }, {
         iconLayout: 'default#image',
         iconImageHref: require('@/assets/img/contacts/marker.svg'),
-        iconImageSize: window.innerWidth > 1024 ? [43, 72] : [29, 48],
-        iconImageOffset: window.innerWidth > 1024 ? [-30, -80] : [-10, -40],
+        iconImageSize: window.innerWidth > 1024 ? [31, 45] : [24, 35],
+        iconImageOffset: window.innerWidth > 1024 ? [-10, -60] : [-10, -40],
       })
 
       map.geoObjects.add(redForest)
@@ -59,12 +56,66 @@ window.addEventListener('load', () => {
     })
 
     // contacts switcher
-    const switchBtns = document.querySelectorAll('.contacts__switcher-btn')
+    const switcher = document.querySelector('.contacts__switcher')
+    const switchItems = document.querySelectorAll('.contacts__content-item')
+    const switchBtns = [...document.querySelectorAll('.contacts__switcher-btn')]
+    const winWidth = window.innerWidth
 
-    function switchContent(e) {
+    const activateContent = (arr, id) => {
+      arr.forEach(el => {
+        if (el.dataset.id === id) {
+          el.classList.add('active')
 
+          if (el.classList.contains('contacts__content-item')) {
+            anime({
+              targets: el,
+              opacity: [0, 1],
+              duration: 1000,
+              easing: 'easeOutQuart'
+            })
+          }
+        } else {
+          el.classList.remove('active')
+        }
+      })
     }
 
-    switchBtns.forEach(el => el.addEventListener('click', switchContent))
+
+    function switchContent(e) {
+      if (window.innerWidth > 1024) {
+        const btn = e.target.closest('.contacts__switcher-btn')
+        const id = btn.dataset.id
+
+        activateContent(switchBtns, id)
+        activateContent(switchItems, id)
+      } else {
+        if (!switcher.classList.contains('open')) {
+          switcher.classList.add('open')
+          switcher.style.maxHeight = `${switcher.scrollHeight}px`
+        } else {
+          const btn = e.target.closest('.contacts__switcher-btn')
+          switcher.classList.remove('open')
+          switcher.removeAttribute('style')
+
+          if (!btn.classList.contains('active')) {
+            const id = btn.dataset.id
+            const prevBtn = switchBtns.find(el => el.classList.contains('active'))
+
+            activateContent(switchBtns, id)
+            activateContent(switchItems, id)
+
+            switcher.insertAdjacentElement('afterbegin', switchBtns[+id])
+          }
+        }
+      }
+    }
+
+    switcher.addEventListener('click', switchContent)
+
+    window.addEventListener('resize', () => {
+      if (winWidth !== window.innerWidth) {
+
+      }
+    })
   }
 });
