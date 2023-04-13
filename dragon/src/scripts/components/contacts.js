9e2aa8cd432f6bd17aad7189ecf01758d1388b3d
Anton Bakhmutsky <anton_bakh@mail.ru>
2023-04-13 08:23:28 +0300

    clear working tree

--- dragon/src/scripts/components/contacts.js
+++ dragon/src/scripts/components/contacts.js
@@ -1,10 +1,13 @@
+import Swiper, {Navigation} from 'swiper'
+
 window.addEventListener('load', () => {
   if (!document.querySelector('.contacts')) {
     return null
   } else {
+
+    // maps
     ymaps.ready(function () {
-      // desktop maps
-      const map = new ymaps.Map('map', {
+      const map = new ymaps.Map('mapRedForest', {
         center: [59.66768456450318, 30.651756999999925],
         zoom: 13,
         controls: [],
@@ -24,5 +27,44 @@ window.addEventListener('load', () => {
 
       map.geoObjects.add(redForest)
     });
+
+    // contacts slider
+    const swipers = document.querySelectorAll('.contacts__list.swiper')
+
+    swipers.forEach(el => {
+      new Swiper(el, {
+        modules: [Navigation],
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+          320: {
+            slidesPerView: 1
+          },
+          1024: {
+            slidesPerView: 2
+          },
+          1400: {
+            slidesPerView: 3
+          },
+          1520: {
+            slidesPerView: 2
+          },
+          1720: {
+            slidesPerView: 3
+          }
+        }
+      })
+    })
+
+    // contacts switcher
+    const switchBtns = document.querySelectorAll('.contacts__switcher-btn')
+
+    function switchContent(e) {
+
+    }
+
+    switchBtns.forEach(el => el.addEventListener('click', switchContent))
   }
 });
