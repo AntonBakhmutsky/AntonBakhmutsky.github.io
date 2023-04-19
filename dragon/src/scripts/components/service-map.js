22f0a36f9340e2677c7bed94504ead1e1957bcac
KrisKipper <yarevyakinakristina@gmail.com>
2023-04-19 14:44:17 +0300

    added  loop of maps

--- dragon/src/scripts/components/service-map.js
+++ dragon/src/scripts/components/service-map.js
@@ -1,65 +1,61 @@
+import {maps} from '../helpers/map';
+
 window.addEventListener('load', () => {
   if (!document.querySelector('#serviceMap')) {
-    return null
+      return null
   } else {
+      main();
+      async function main() {
+          await ymaps3.ready
+          const {
+              YMap,
+              YMapDefaultSchemeLayer,
+              YMapMarker,
+              YMapControls,
+              YMapDefaultFeaturesLayer
+          } = ymaps3
 
-    main();
-    async function main() {
-      await ymaps3.ready
-      const {
-        YMap,
-        YMapDefaultSchemeLayer,
-        YMapMarker,
-        YMapControls,
-        YMapDefaultFeaturesLayer
-      } = ymaps3
+          const {YMapZoomControl, YMapGeolocationControl} = await ymaps3.import('@yandex/ymaps3-controls@0.0.1');
+          const {YMapOpenMapsButton} = await ymaps3.import('@yandex/ymaps3-controls-extra')
 
-      const {YMapZoomControl, YMapGeolocationControl} = await ymaps3.import('@yandex/ymaps3-controls@0.0.1');
-      const {YMapOpenMapsButton} = await ymaps3.import('@yandex/ymaps3-controls-extra')
+          const serviceMap = new YMap(document.getElementById('serviceMap'), {
+              location: {
+                  center: [30.27519509621456, 59.86463810516632],
+                  zoom: 10,
+              },
+              mode: 'vector'
+          })
+          serviceMap.addChild(new YMapControls({position: 'right'}).addChild(new YMapZoomControl({})))
+          serviceMap.addChild(new YMapDefaultFeaturesLayer({id: 'features'}))
+          serviceMap.addChild(new YMapControls({position: 'left'}).addChild(new YMapGeolocationControl({})))
+          serviceMap.addChild(new YMapControls({position: 'top left'}).addChild(new YMapOpenMapsButton({})))
 
-      const serviceMap = new YMap(document.getElementById('serviceMap'), {
-        location: {
-          center: [30.27519509621456, 59.86463810516632],
-          zoom: 10,
-        },
-        mode: 'vector'
-      })
+          const layer = new YMapDefaultSchemeLayer({
+              customization: [
+                {
+                  tags: {
+                      all: ['water', 'road', 'landscape'],
+                  },
+                  elements: 'geometry',
+                  stylers: [
+                    {
+                      opacity: 0
+                  }
+                ]
+              }
+            ]
+          })
 
-      serviceMap.addChild(new YMapControls({position: 'right'}).addChild(new YMapZoomControl({})))
-      serviceMap.addChild(new YMapDefaultFeaturesLayer({id: 'features'}))
-      serviceMap.addChild(new YMapControls({position: 'left'}).addChild(new YMapGeolocationControl({})))
-      serviceMap.addChild(new YMapControls({position: 'top left'}).addChild(new YMapOpenMapsButton({})))
-
-      const layer = new YMapDefaultSchemeLayer({
-                      customization: [
-                        {
-                          tags: {
-                            all: ['water', 'road', 'landscape'],
-                          },
-                          elements: 'geometry',
-                          stylers: [
-                            {
-                              opacity: 0
-                            }
-                          ]
-                        }
-                      ]
-                    })
-
-      serviceMap.addChild(layer)
-
-      const serviceContent = document.createElement('div')
-      const shopContent = document.createElement('div')
-      serviceContent.innerHTML = `<div class="map__marker map__marker_service"></div>`
-      shopContent.innerHTML = `<div class="map__marker map__marker_shop"></div>`
-
-      serviceMap.addChild(new YMapMarker({
-        coordinates: [30.651756999999925, 59.66768456450318],
-      }, serviceContent))
-
-      serviceMap.addChild(new YMapMarker({
-        coordinates: [30.651756999999925, 59.66768456450318],
-      }, serviceContent))
-    }
+          serviceMap.addChild(layer)
+          
+          for (const map of maps) {
+              const markerContent = document.createElement('div');
+              markerContent.innerHTML = `<div class="map__marker map__marker_${map.modifier}"></div>`;
+              const marker = new YMapMarker({
+                  coordinates: map.coordinates,
+              }, markerContent);
+              serviceMap.addChild(marker);
+          }
+      }
   }
 });
