5208898e61157dd2a1eefb8607f37490104e71fd
Anton Bakhmutsky <anton_bakh@mail.ru>
2023-04-21 08:14:57 +0300

    clear working tree

--- dragon/src/scripts/components/news-page-slider.js
+++ dragon/src/scripts/components/news-page-slider.js
@@ -1,28 +1,27 @@
-import Swiper, { Navigation, Pagination, Mousewheel, Autoplay } from 'swiper';
+import Swiper, {Navigation, Pagination, Mousewheel, Autoplay} from 'swiper';
 
 
 window.addEventListener('load', () => {
-    if (!document.querySelector('.news-page__swiper-wrapper')) {
-      return false
-    } else {
+  if (!document.querySelector('.news-page__swiper-wrapper')) {
+    return false
+  } else {
 
-const swiper = new Swiper('.swiper-container', {
-  modules: [Navigation, Pagination, Mousewheel, Autoplay],
-  loop: true,
-  slidesPerView: 1,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  autoplay: {
-    delay: 3000,
-  },
-});
+    const swiper = new Swiper('.swiper-container', {
+      modules: [Navigation, Pagination, Mousewheel, Autoplay],
+      slidesPerView: 1,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      autoplay: {
+        delay: 3000,
+      },
+    });
 
 
-}
+  }
 })
