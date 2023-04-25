6b737295bb113cb4308ca5e22bf5f4c166242a89
KrisKipper <yarevyakinakristina@gmail.com>
2023-04-25 16:48:22 +0300

    map

--- trucks-point/src/scripts/components/map.js
+++ trucks-point/src/scripts/components/map.js
@@ -1,7 +1,8 @@
 import L from 'leaflet';
 
-  var map = L.map('map').setView([51.505, -0.09], 13);
+let map = L.map('map').setView([51.505, -0.09], 13);
+
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-  maxZoom: 18,
+  maxZoom: 19,
+  attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
 }).addTo(map);
