6b9b71d9efedcdadc466b1a604bb7e623d4c9271
KrisKipper <yarevyakinakristina@gmail.com>
2023-04-28 17:20:33 +0300

    Added: menu toggle

---
+++ trucks-point/src/scripts/components/menu.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', () => {
+  if (!document.querySelector('.application')) {
+    return null
+  } else {
+    const menuToggle = document.querySelector('#menu-toggle');
+    const menu = document.querySelector('.menu');
+    const menuNavName = document.querySelector('.list__text');
+    const btnToggleImg = document.querySelector('.menu__toggle_img');
+
+    menuToggle.addEventListener('click', function() {
+      menu.classList.toggle('menu-min');
+      menuNavName.classList.toggle('hide-list__text')
+    });
+
+    menuToggle.click();
+  }
+});
