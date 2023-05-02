b869c09792c82bf6afccca15e32dfcdd3960f083
KrisKipper <yarevyakinakristina@gmail.com>
2023-05-02 12:44:47 +0300

    Fixed bags

--- trucks-point/src/scripts/components/application.js
+++ trucks-point/src/scripts/components/application.js
@@ -8,22 +8,26 @@ window.addEventListener('load', () => {
     const applicationToggle = document.getElementById("applicationToggle");
     const btnReset = document.getElementById("application-btn-reset");
     const longFormTitle = document.querySelector(".application_search_toggle__title");
-    let isShortFormVisible = false;
+    const searchForm = document.querySelector(".application_search");
+    let isShortFormVisible = true;
 
     applicationToggle.addEventListener("click", ()=>{
+
       if (!isShortFormVisible) {
         shortForm.style.display = 'block';
         longForm.style.display = 'none';
         btnReset.style.display = 'none';
         longFormTitle.style.display = 'none';
         isShortFormVisible = true;
+        searchForm.classList.toggle('application_search-long-type');
       } else {
         // Если элемент 1 уже показан, скрываем его и показываем элемент 2
         shortForm.style.display = 'none';
         longForm.style.display = 'block';
-        btnReset.style.display = 'block';
+        btnReset.style.display = 'flex';
         longFormTitle.style.display = 'block';
         isShortFormVisible = false;
+        searchForm.classList.toggle('application_search-long-type');
       }
     });
   }
--- trucks-point/src/scripts/components/map.js
+++ trucks-point/src/scripts/components/map.js
@@ -1,9 +1,17 @@
 import L from 'leaflet';
 
 
-let map = L.map('map').setView([51.505, -0.09], 13);
+window.addEventListener('load', () => {
+  if (!document.querySelector('.map')) {
+    return null
+  } else {
+    let map = L.map('map').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
-}).addTo(map);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+  }
+
+});
