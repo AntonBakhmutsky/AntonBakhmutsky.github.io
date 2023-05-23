dd640626575ba3332e689ae92f73fb749e3f7d60
KrisKipper <yarevyakinakristina@gmail.com>
2023-05-23 15:02:08 +0300

    Created trucks page

--- trucks-point/src/scripts/main.js
+++ trucks-point/src/scripts/main.js
@@ -1,6 +1,7 @@
 require('./components/menu')
 require('./components/home')
 require('./components/notice')
+require('./components/trucks')
 require('./components/application')
 require('./components/one-application')
 require('./components/one-application-creating')
