a893ab9a39f0918330246c2296a3274eadf35cf6
KrisKipper <yarevyakinakristina@gmail.com>
2023-05-24 16:27:17 +0300

    Added modal

--- factory/src/scripts/components/index-form.js
+++ factory/src/scripts/components/index-form.js
@@ -29,4 +29,53 @@ window.addEventListener('load', () => {
     })
 
 
+
+    const submitBtn = document.querySelector('.index-form-content-btn')
+    const modalSent = document.querySelector('.sent-modal')
+    const nameInput = document.getElementById("indexFormInputName");
+    const phoneInput = document.getElementById("indexFormInputTel");
+    const emailInput = document.getElementById("indexFormInputEmail");
+    const form = document.getElementById('indexForm')
+    const btnCloseModal = document.getElementById('sentModalClose')
+
+    function closeModal() {
+      console.log('hf')
+      modalSent.style.display = 'none';
+    }
+
+    btnCloseModal.addEventListener('click', closeModal)
+
+    modalSent.addEventListener('click', function(e) {
+      if (e.target === modalSent) {
+        closeModal();
+      }
+    });
+
+    function validateEmail () {
+      const emailV = emailInput.value;
+      const emailPattern = /\S+@\S+\.\S+/; // используем проверку наличия символа @ и точки после него
+
+      return emailPattern.test(emailV);
+    }
+    function showModal () {
+      if (nameInput.value === "") {
+        console.log("Введите имя");
+      } else if (phoneInput.value === "") {
+        console.log("Введите номер телефона");
+      }  else if (emailInput.value === "") {
+        console.log("Введите email");
+      } else if (!validateEmail()) {
+        console.log("Введён некорректный email");
+      } else {
+        modalSent.style.display = 'flex';
+        form.reset();
+      }
+    }
+
+    submitBtn.addEventListener('click', showModal)
+
+
+
+
+
 }});
