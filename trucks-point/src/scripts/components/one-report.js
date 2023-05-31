1a6d78fd80e8ca49965426d209351d33b4a6ba05
KrisKipper <yarevyakinakristina@gmail.com>
2023-05-31 11:28:16 +0300

    Fixed toggle function

--- trucks-point/src/scripts/components/one-report.js
+++ trucks-point/src/scripts/components/one-report.js
@@ -23,30 +23,44 @@ window.addEventListener('load', () => {
 // Для каждого элемента А добавляем обработчик события
     elementsA.forEach(function(elementA) {
       const toggleButtonA = elementA.querySelector(".one-report-list-item-title-logist__btn");
+      const minusA = elementA.querySelector(".one-report-list-item-title-logist__btn_minus");
+      const plusA = elementA.querySelector(".one-report-list-item-title-logist__btn_plus");
 
       toggleButtonA.addEventListener("click", function() {
         const elementBs = elementA.querySelectorAll(".one-report-list-item-subtitle");
 
+
         elementBs.forEach(function(elementB) {
           const toggleButtonB = elementB.querySelector(".one-report-list-item-subtitle-car__btn");
           const elementC = elementB.querySelectorAll(".one-report-list-item-subtitle-info");
+          const plusB = elementB.querySelectorAll(".one-report-list-item-subtitle-car__btn_plus");
+          const minusB = elementB.querySelectorAll(".one-report-list-item-subtitle-car__btn_minus");
 
           toggleButtonB.addEventListener("click", function(event) {
             elementC.forEach(function(elementC) {
               elementC.classList.toggle("block");
             });
 
-            toggleButtonB.classList.toggle("active");
+            plusB.forEach(function(plB) {
+              plB.classList.toggle("none");
+            });
+
+            minusB.forEach(function(mnB) {
+              mnB.classList.toggle("block");
+            });
+
+
+            toggleButtonB.classList.toggle("orange-border");
           });
 
           elementB.classList.toggle("block");
-          elementC.forEach(function(elementC) {
-            elementC.classList.remove("block");
-          });
 
         });
 
-        toggleButtonA.classList.toggle("active");
+        elementA.classList.toggle("gray-background");
+        toggleButtonA.classList.toggle("orange-border");
+        plusA.classList.toggle("none");
+        minusA.classList.toggle("block");
       });
     });
 
