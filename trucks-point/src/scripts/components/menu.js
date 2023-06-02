4212b340373ad06c98e16b4bc5931f8920950af4
KrisKipper <yarevyakinakristina@gmail.com>
2023-06-02 08:27:36 +0300

    Changed menu styles

--- trucks-point/src/scripts/components/menu.js
+++ trucks-point/src/scripts/components/menu.js
@@ -4,14 +4,15 @@ window.addEventListener('load', () => {
   } else {
     const menuToggle = document.querySelector('#menu-toggle');
     const menu = document.querySelector('.menu');
+    const menuContainer = document.querySelector('.menu-container');
     const menuNavName = document.querySelector('.list__text');
     const btnToggleImg = document.querySelector('.menu__toggle_img');
 
     menuToggle.addEventListener('click', function() {
-      menu.classList.toggle('menu-min');
+      menu.classList.toggle('menu-max');
       menuNavName.classList.toggle('hide-list__text')
+      menuContainer.classList.toggle('menu-container-max')
     });
 
-    menuToggle.click();
   }
 });
