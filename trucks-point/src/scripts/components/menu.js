eea8ab6c4ca93efd55dda8b35503967ebb8b2e5a
KrisKipper <yarevyakinakristina@gmail.com>
2023-06-05 08:11:11 +0300

    Added toggle function

--- trucks-point/src/scripts/components/menu.js
+++ trucks-point/src/scripts/components/menu.js
@@ -7,12 +7,24 @@ window.addEventListener('load', () => {
     const menuContainer = document.querySelector('.menu-container');
     const menuNavName = document.querySelector('.list__text');
     const btnToggleImg = document.querySelector('.menu__toggle_img');
+    const mainContent = document.querySelector("main");
 
     menuToggle.addEventListener('click', function() {
       menu.classList.toggle('menu-max');
       menuNavName.classList.toggle('hide-list__text')
       menuContainer.classList.toggle('menu-container-max')
+      mainContent.classList.toggle('content-open')
     });
+
+
+    // function toggleMenu() {
+    //   if (menu.classList.contains("menu-max")) {
+    //     mainContent.classList.remove("content-open");
+    //   } else {
+    //     menu.classList.add("menu-open");
+    //     mainContent.classList.add("menu-open");
+    //   }
+    // }
 
   }
 });
