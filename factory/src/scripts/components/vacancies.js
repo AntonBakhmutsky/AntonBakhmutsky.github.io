4095f191c24e8f791a44f9def036ac3adb008398
Anton Bakhmutsky <anton_bakh@mail.ru>
2023-07-29 16:05:43 +0300

    clear working tree

--- factory/src/scripts/components/vacancies.js
+++ factory/src/scripts/components/vacancies.js
@@ -1,3 +1,6 @@
+import initInputs from '@/scripts/helpers/inputs'
+import PhoneMask from '@zoibana/phonemask'
+
 window.addEventListener('load', () => {
   if (!document.querySelector('.main_vacancies')) {
     return false
@@ -17,6 +20,41 @@ window.addEventListener('load', () => {
       }
 
       container.addEventListener('click', toggleAccordion)
+    }
+
+    if (document.querySelector('.form__input')) {
+      initInputs(document.querySelectorAll('.form__input'))
+    }
+
+    if (document.querySelector('input[type="tel"]')) {
+      const phoneInputs = document.querySelectorAll('.modal input[type="tel"]')
+      phoneInputs.forEach(el => new PhoneMask(el))
+    }
+
+    if (document.querySelector('.form__file')) {
+      const fileInput = document.querySelector('.modal input[type="file"]')
+      const fileInputClear = document.querySelector('.modal .form__input-clear')
+
+      function toggleFile(e) {
+          const container = this.closest('.form__file')
+          const currentValue = container.querySelector('span')
+          const allowedExtensions = ['pdf', 'docx', 'doc', 'pptx']
+          const extension = this.value.split('.').at(-1).trim().toLowerCase()
+
+          if (this.files.length && allowedExtensions.includes(extension)) {
+
+            container.classList.add('fill')
+            currentValue.textContent = this.files[0].name
+
+          } else {
+            container.classList.add('error')
+            setTimeout(() => {
+              container.classList.remove('error')
+            }, 5000)
+          }
+      }
+
+      fileInput.addEventListener('change', toggleFile)
     }
   }
 })
