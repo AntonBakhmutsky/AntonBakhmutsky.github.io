82abbd98758eb33b655f82e10b34161e72204e48
Anton Bakhmutsky <anton_bakh@mail.ru>
2023-08-15 15:40:42 +0300

    clear working tree

--- alma/src/scripts/components/global.js
+++ alma/src/scripts/components/global.js
@@ -2,7 +2,6 @@ import initInputs from '../helpers/inputs'
 
 window.addEventListener('load', () => {
   if (document.querySelector('.form-input')) {
-    console.log(true)
     initInputs(document.querySelectorAll('.form-input'))
   }
 })
