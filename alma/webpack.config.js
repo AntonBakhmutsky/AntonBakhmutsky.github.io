c85ce11779cfb00021e52fead91562f1f0eb8762
Anton Bakhmutsky <anton_bakh@mail.ru>
2023-08-15 07:59:19 +0300

    fix

--- alma/webpack.config.js
+++ alma/webpack.config.js
@@ -14,7 +14,7 @@ const pages = fs.readdirSync(pagesDir).filter(fileName => fileName.endsWith('.pu
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   entry: {
-    app: ['@babel/polyfill', './global.js']
+    app: ['@babel/polyfill', './app.js']
   },
   output: {
     filename: '[name].js',
