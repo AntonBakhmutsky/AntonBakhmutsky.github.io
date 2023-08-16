e84c3364d593c09b8c52fd915a0ac9871ad64884
Anton Bakhmutsky <anton_bakh@mail.ru>
2023-08-16 15:04:08 +0300

    clear working tree

--- alma/src/scripts/components/header.js
+++ alma/src/scripts/components/header.js
@@ -4,27 +4,43 @@ window.addEventListener('load', () => {
     const bottomMenuLinks = document.querySelectorAll('.header__bottom-menu a')
     const hiddenMenu = document.querySelector('.header__hidden')
     const hiddenMenuItems = [...hiddenMenu.children]
+    const hiddenMenuLinks = hiddenMenu.querySelectorAll('.header__hidden-links-item')
+    const hiddenMenuButtons = hiddenMenu.querySelectorAll('.header__hidden-btn')
+    const overlay = document.querySelector('.overlay')
 
-    const clearLinks = () => bottomMenuLinks.forEach(el => el.classList.remove('active'))
+    const clearItems = (items) => items.forEach(el => el.classList.remove('active'))
 
     function toggleHiddenMenu() {
       if (!this.classList.contains('active')) {
-        clearLinks()
+        clearItems(bottomMenuLinks)
         this.classList.add('active')
         hiddenMenu.classList.add('active')
+        hiddenMenuItems.forEach(el => el.classList.remove('active'))
+        const menuItem = hiddenMenuItems.find(el => el.dataset.id === this.dataset.id)
+        menuItem.classList.add('active')
+        menuItem.querySelector('.header__hidden-links-item').classList.add('active')
+        overlay.classList.add('active')
       } else {
         closeHiddenMenu()
       }
     }
 
     function closeHiddenMenu() {
-      clearLinks()
+      clearItems(bottomMenuLinks)
+      clearItems(hiddenMenuItems)
+      clearItems(hiddenMenuLinks)
+      hiddenMenu.classList.remove('active')
+      overlay.classList.remove('active')
     }
 
-    function toggleMenuLinks() {
-
+    function toggleMenuButtons() {
+      const links = [...this.parentElement.nextElementSibling.children]
+      links.forEach(el => el.classList.remove('active'))
+      links[+this.dataset.id].classList.add('active')
     }
 
     bottomMenuLinks.forEach(el => el.addEventListener('click', toggleHiddenMenu))
+    hiddenMenuButtons.forEach(el => el.addEventListener('mouseover', toggleMenuButtons))
+    overlay.addEventListener('click', closeHiddenMenu)
   }
 })
