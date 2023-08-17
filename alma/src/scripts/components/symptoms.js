40c999ba228196e3ef0c58e4e850c0d8e199c15d
KrisKipper <yarevyakinakristina@gmail.com>
2023-08-17 14:00:37 +0300

    fix symptoms script

--- alma/src/scripts/components/symptoms.js
+++ alma/src/scripts/components/symptoms.js
@@ -1,13 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const toggleButton = document.querySelector(".symptoms_toggle-btn");
-  const hiddenLinks = document.querySelectorAll(".symptoms-link.hidden");
+window.addEventListener('load', () => {
+  if (document.querySelector('.symptoms')) {
 
-  toggleButton.addEventListener("click", function() {
-    hiddenLinks.forEach(link => {
-      link.style.display = link.style.display === "none" ? "inline" : "none";
+    const toggleButton = document.querySelector(".symptoms_toggle-btn");
+    const hiddenLinks = document.querySelectorAll(".symptoms-link.hidden");
+
+    toggleButton.addEventListener("click", function() {
+      hiddenLinks.forEach(link => {
+        link.style.display = link.style.display === "none" ? "inline" : "none";
+      });
+
+      toggleButton.textContent = hiddenLinks[0].style.display === "none" ? "Показать ещё" : "Свернуть";
     });
 
-    toggleButton.textContent = hiddenLinks[0].style.display === "none" ? "Показать ещё" : "Свернуть";
-  });
-});
+  }
+})
 
