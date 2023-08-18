2bc887dc5dae8f956c2560f064c1e12e47965d77
KrisKipper <yarevyakinakristina@gmail.com>
2023-08-18 15:43:07 +0300

    tablet sm styles

--- alma/src/scripts/components/doctors.js
+++ alma/src/scripts/components/doctors.js
@@ -1,14 +1,27 @@
 import Swiper from 'swiper'
 import {Navigation} from 'swiper/modules'
 
+
+
+const screenWidth = window.innerWidth;
+
+if (screenWidth <= 768) {
+  const countCard = 3;
+} else {
+  const countCard = 4;
+}
+
 window.addEventListener('load', () => {
   new Swiper('.doctors-slider .swiper', {
     modules: [Navigation],
-    slidesPerView: 4,
+    slidesPerView: countCard,
     spaceBetween: 24,
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     }
+
   })
+
+
 })
