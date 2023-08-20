5d8c1e0f2cbc743f11f1818246fa2f0bc2abc452
Kristina <yarevyakinakristina@gmail.com>
2023-08-20 18:12:08 +0300

    add mobile styles

--- alma/src/scripts/components/doctors.js
+++ alma/src/scripts/components/doctors.js
@@ -1,13 +1,16 @@
 import Swiper from 'swiper';
-import { Navigation } from 'swiper/modules';
+import {Navigation, Pagination} from 'swiper/modules';
 
 let swiperInstance;
 
 function initSwiper() {
   swiperInstance = new Swiper('.doctors-slider .swiper', {
-    modules: [Navigation],
+    modules: [Navigation, Pagination],
     slidesPerView: 4,
     spaceBetween: 24,
+    pagination: {
+      el: '.swiper-pagination',
+    },
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
@@ -18,7 +21,13 @@ function initSwiper() {
 function updateSwiper() {
   const screenWidth = window.innerWidth;
 
-  if (screenWidth <= 768) {
+  if (screenWidth <= 576) {
+    if (swiperInstance.params.slidesPerView !== 1) {
+      swiperInstance.params.slidesPerView = 1;
+      swiperInstance.params.spaceBetween = 20; // Можете установить spaceBetween в 0 или другое значение по вашему выбору.
+      swiperInstance.update();
+    }
+  } else if (screenWidth <= 768) {
     if (swiperInstance.params.slidesPerView !== 3) {
       swiperInstance.params.slidesPerView = 3;
       swiperInstance.params.spaceBetween = 20;
@@ -41,3 +50,4 @@ window.addEventListener('load', () => {
 window.addEventListener('resize', () => {
   updateSwiper();
 });
+
--- alma/src/scripts/components/symptoms.js
+++ alma/src/scripts/components/symptoms.js
@@ -12,6 +12,23 @@ window.addEventListener('load', () => {
       toggleButton.textContent = hiddenLinks[0].style.display === "none" ? "Показать ещё" : "Свернуть";
     });
 
+    function updateSymptomsVisibility() {
+      const screenWidth = window.innerWidth;
+      const symptomsLinks = document.querySelectorAll('.symptoms-link');
+
+      symptomsLinks.forEach(function(link, index) {
+        if (screenWidth < 576) {
+          link.classList.toggle('hidden', index > 5);
+        } else {
+          link.classList.remove('hidden');
+        }
+      });
+    }
+
+    // Вызываем функцию при загрузке страницы и изменении размера окна
+    updateSymptomsVisibility();
+    window.addEventListener('resize', updateSymptomsVisibility);
+
   }
 })
 
