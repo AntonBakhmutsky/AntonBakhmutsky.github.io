629c1fc6e5fb791686d76a158f1b5fb33f02fa78
Anton Bakhmutsky <anton_bakh@mail.ru>
2023-08-22 12:14:46 +0300

    clear working tree

--- alma/src/scripts/components/about.js
+++ alma/src/scripts/components/about.js
@@ -1,13 +1,21 @@
 import Swiper from 'swiper'
 import {Navigation} from 'swiper/modules'
+import initModalImage from '@/scripts/helpers/modalImage'
 
 window.addEventListener('load', () => {
-  new Swiper('.about-slider .swiper', {
-    modules: [Navigation],
-    loop: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
-  })
+  if (!document.querySelector('.about-slider')) {
+    return false
+  } else  {
+    new Swiper('.about-slider .swiper', {
+      modules: [Navigation],
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      }
+    })
+
+    const slides = document.querySelectorAll('.about-slider .swiper-slide')
+    initModalImage(slides)
+  }
 })
