0cb7460f2fc9ab0e0f6d1889c34acae790258439
KrisKipper <yarevyakinakristina@gmail.com>
2023-08-22 11:17:09 +0300

    about slider

--- alma/src/scripts/components/about.js
+++ alma/src/scripts/components/about.js
@@ -1,10 +1,22 @@
 import Swiper from 'swiper'
-import {Navigation} from 'swiper/modules'
+import {Navigation, Pagination} from 'swiper/modules'
 
 window.addEventListener('load', () => {
   new Swiper('.about-slider .swiper', {
     modules: [Navigation],
     loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  })
+
+  new Swiper('.about-slider-mobile .swiper', {
+    modules: [Navigation, Pagination],
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+    },
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
