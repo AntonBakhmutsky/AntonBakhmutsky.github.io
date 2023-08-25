53c6ab67a199ef2f57aeb147b89136cc868702b2
KrisKipper <yarevyakinakristina@gmail.com>
2023-08-25 09:55:02 +0300

    fix

--- alma/src/scripts/components/reviews.js
+++ alma/src/scripts/components/reviews.js
@@ -5,12 +5,23 @@ window.addEventListener('load', () => {
 
     reviewsCards.forEach(card => {
       const btn = card.querySelector('.expand-btn')
+      const reviewsText = card.querySelector('.reviews_text')
+      const textHeight = reviewsText.clientHeight
+      const lineHeight = parseInt(window.getComputedStyle(reviewsText).lineHeight)
+      const numberOfLines = textHeight / lineHeight
+
+      if (numberOfLines < 6) {
+        card.classList.add('active')
+        btn.style.display = 'none'
+      }
 
       btn.addEventListener('click', () => {
-        card.classList.toggle("active")
-        btn.textContent = card.classList.contains("active") ? "Свернуть" : "Развернуть";
-
+        card.classList.toggle('active')
+        btn.textContent = card.classList.contains('active') ? 'Свернуть' : 'Развернуть'
       });
     });
   }
-})
+});
+
+
+
