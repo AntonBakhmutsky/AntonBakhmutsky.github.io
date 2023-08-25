6f7a5990b29519b945e6d8cd80ccdc4850ac92fc
KrisKipper <yarevyakinakristina@gmail.com>
2023-08-25 15:41:24 +0300

    about mobile styles

--- alma/src/scripts/components/slider.js
+++ alma/src/scripts/components/slider.js
@@ -1,53 +1,71 @@
 import Swiper from 'swiper';
-import {Navigation, Pagination} from 'swiper/modules';
+import { Navigation, Pagination } from 'swiper/modules';
 
-let swiperInstance;
+let doctorsSlider;
+let aboutSlider;
 
-
-function initSwiper() {
-  swiperInstance = new Swiper('.slider-four .swiper', {
+function initSwipers() {
+  doctorsSlider = new Swiper('.doctors-slider .swiper', {
     modules: [Navigation, Pagination],
     slidesPerView: 4,
     spaceBetween: 24,
     pagination: {
-      el: '.swiper-pagination',
+      el: '.doctors-slider .swiper-pagination',
     },
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
+      nextEl: '.doctors-slider .swiper .swiper-button-next',
+      prevEl: '.doctors-slider .swiper .swiper-button-prev',
+    },
+  });
+
+  aboutSlider = new Swiper('.about-main-slider .swiper', {
+    modules: [Navigation, Pagination],
+    slidesPerView: 3,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: '.about-main-slider .swiper .swiper-button-next',
+      prevEl: '.about-main-slider .swiper .swiper-button-prev',
+    },
   });
 }
 
-function updateSwiper() {
+function updateSwipers() {
   const screenWidth = window.innerWidth;
 
   if (screenWidth <= 576) {
-    if (swiperInstance.params.slidesPerView !== 1) {
-      swiperInstance.params.slidesPerView = 1;
-      swiperInstance.params.spaceBetween = 20; // Можете установить spaceBetween в 0 или другое значение по вашему выбору.
-      swiperInstance.update();
-    }
+    doctorsSlider.params.slidesPerView = 1;
+    doctorsSlider.params.spaceBetween = 20;
+    doctorsSlider.update();
+
+    aboutSlider.params.slidesPerView = 1;
+    aboutSlider.params.spaceBetween = 20;
+    aboutSlider.update();
   } else if (screenWidth <= 768) {
-    if (swiperInstance.params.slidesPerView !== 3) {
-      swiperInstance.params.slidesPerView = 3;
-      swiperInstance.params.spaceBetween = 20;
-      swiperInstance.update();
-    }
+    doctorsSlider.params.slidesPerView = 3;
+    doctorsSlider.params.spaceBetween = 20;
+    doctorsSlider.update();
+
+    aboutSlider.params.slidesPerView = 3;
+    aboutSlider.params.spaceBetween = 20;
+    aboutSlider.update();
   } else {
-    if (swiperInstance.params.slidesPerView !== 4) {
-      swiperInstance.params.slidesPerView = 4;
-      swiperInstance.params.spaceBetween = 24;
-      swiperInstance.update();
-    }
+    doctorsSlider.params.slidesPerView = 4;
+    doctorsSlider.params.spaceBetween = 24;
+    doctorsSlider.update();
+
+    aboutSlider.params.slidesPerView = 4;
+    aboutSlider.params.spaceBetween = 24;
+    aboutSlider.update();
   }
 }
 
-window.addEventListener('load', () => {
-  initSwiper();
-  updateSwiper();
+window.addEventListener('DOMContentLoaded', () => {
+  initSwipers();
+  updateSwipers();
 });
 
 window.addEventListener('resize', () => {
-  updateSwiper();
+  updateSwipers();
 });
+
+
