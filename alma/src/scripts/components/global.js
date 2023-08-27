7d80458b73263bcf6b7d62aa17ba7430ef412fcc
Anton Bakhmutsky <anton_bakh@mail.ru>
2023-08-27 17:42:36 +0300

    added anchor script

--- alma/src/scripts/components/global.js
+++ alma/src/scripts/components/global.js
@@ -11,4 +11,24 @@ window.addEventListener('load', () => {
       Inputmask({'mask': '+7 (999) 999-99-99'}).mask(el)
     })
   }
+
+  // якоря
+  if (document.querySelector('a[href^="#"]')) {
+    const anchors = document.querySelectorAll('a[href^="#"]')
+
+    function goToAnchor(e) {
+      e.preventDefault()
+      if (e.target.closest('.price-anchors-mobile')) {
+        document.querySelectorAll('.price-anchors-mobile a').forEach(el => el.classList.remove('active'))
+        this.classList.add('active')
+      }
+      const el = document.querySelector(`#${this.href.split('#').at(-1)}`)
+      const y = el.getBoundingClientRect().top
+      window.scrollBy({
+        top: y - document.querySelector('.header').offsetHeight - 15
+      })
+    }
+
+    anchors.forEach(el => el.addEventListener('click', goToAnchor))
+  }
 })
