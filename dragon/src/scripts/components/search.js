08ae4a3517d74fa709873f3c7626c7079e0550f3
Anton Bakhmutsky <anton_bakh@mail.ru>
2023-10-03 08:20:51 +0300

    clear working tree

--- dragon/src/scripts/components/search.js
+++ dragon/src/scripts/components/search.js
@@ -1,13 +1,17 @@
 window.addEventListener('load', () => {
   if (document.querySelector('.main_search')) {
-    if (!sessionStorage.getItem('mobileSearchEnter')) {
-      sessionStorage.setItem('mobileSearchEnter', 'done')
-      document.querySelector('.search-results-text').remove()
+    const resultTxt = document.querySelector('.search-results-text')
+    const items = document.querySelector('.search-results .container')
+    if (!localStorage.getItem('mobileSearchEnter') && items.children.length - 1 <= 0) {
+      localStorage.setItem('mobileSearchEnter', 'done')
+      resultTxt.textContent = ''
+    } else {
+      resultTxt.textContent = `Найдено результатов: ${items.children.length - 1}`
     }
   }
   window.addEventListener('beforeunload', (e) => {
     if (document.querySelector('.main_search')) {
-      sessionStorage.removeItem('mobileSearchEnter')
+      localStorage.removeItem('mobileSearchEnter')
     }
   })
 })
